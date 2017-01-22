bd6f2b849ecdd4b70ebf7db00a7d8684c9255a83
Kenny Chen <kc1230@hotmail.com>
2017-01-22 22:31:41 +1100

    progress

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
+//system import
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+//custom import
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}

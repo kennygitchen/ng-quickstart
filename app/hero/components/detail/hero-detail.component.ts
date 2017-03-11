3f702145c03b95f89bf73a2781d3883f8cb0d6c8
Kenny Chen <kc1230@hotmail.com>
2017-03-11 23:51:20 +1100

    implement reactive form - completed

--- app/hero/components/detail/hero-detail.component.ts
+++ app/hero/components/detail/hero-detail.component.ts
@@ -1,55 +1,62 @@
 import 'rxjs/add/operator/switchMap';
 
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Location } from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Location} from '@angular/common';
 
-import { HeroService } from '../../services/hero.service';
+import {HeroService} from '../../services/hero.service';
 //import { Hero } from '../../hero';
 
 @Component({
-    moduleId: module.id,
-    selector: 'my-hero-detail',
-    styleUrls: ['hero-detail.component.css'],
-    templateUrl: 'hero-detail.component.html'
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  styleUrls: ['hero-detail.component.css'],
+  templateUrl: 'hero-detail.component.html'
 })
 
 export class HeroDetailComponent implements OnInit {
 
-    private heroDetailForm: FormGroup;
-    private abilityTypes = ['Physical', 'Regeneration', 'Magic', 'Mental'];
+  private heroDetailForm: FormGroup;
+  private abilityTypes = ['Physical', 'Regeneration', 'Magic', 'Mental'];
 
-    constructor(
-        private heroService: HeroService,
-        private router: ActivatedRoute,
-        private location: Location,
-        private formBuilder: FormBuilder ) {
-    }
+  constructor(private heroService: HeroService,
+              private router: ActivatedRoute,
+              private location: Location,
+              private formBuilder: FormBuilder) {
+  }
 
-    ngOnInit(): void {
-        this.router.params
-            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
-            .subscribe((hero) => {
-              this.heroDetailForm = this.formBuilder.group( {
-                id: hero.id,
-                name: [hero.name, Validators.required],
-                abilityType: [hero.abilityType, Validators.required]
-              } );
-            });
-    }
+  ngOnInit(): void {
+    this.router.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe((hero) => {
+        this.heroDetailForm = this.formBuilder.group({
+          id: hero.id,
+          name: [hero.name, Validators.required],
+          abilityType: [hero.abilityType, Validators.required]
+        });
+        this.heroDetailForm.get('name').valueChanges
+          .debounceTime(500).subscribe(newValue => {
+          console.error("onChange, name=" + newValue);
+        });
+      });
+  }
 
-    save(): void {
-        if( this.heroDetailForm.invalid ) {
-            alert( 'Form contains invalid data.' );
-            return;
-        }
-        this.heroService
-          .saveHero( this.heroDetailForm.getRawValue() )
-          .then(() => this.goBack());
+  save(): void {
+    if (this.heroDetailForm.invalid) {
+      alert('Form contains invalid data.');
+      return;
     }
+    this.heroService
+      .saveHero(this.heroDetailForm.getRawValue())
+      .then(() => this.goBack());
+  }
 
-    goBack(): void {
-        this.location.back();
-    }
+  revert(): void {
+    this.ngOnInit();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
